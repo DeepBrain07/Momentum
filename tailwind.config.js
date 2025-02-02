0cafac5484389330cc5bfb96dd8d9351f9f1356c
DH4NN <olusatimmie07@gmail.com>
2025-02-02 15:51:31 +0100

    Updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html" // Add this in case you have HTML files in public
+  ],
   darkMode: 'class', // or 'media'
-  screens: {
-    xs: '320px',
-    sm: '640px',
-    md: '768px',
-    lg: '1024px',
-    xl: '1280px',
-    '2xl': '1536px',
-  },
   theme: {
     extend: {
       colors: {
         primary: "#013B7D",
-        "bodyText": "#3C3C3C",
-        "backgroundColor": "#F8F8EE",
+        bodyText: "#3C3C3C",
+        backgroundColor: "#F8F8EE",
         secondary: "#5B88FE",
         tertiary: "#0D2154"
       },
